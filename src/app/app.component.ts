33d43663fe6d9b04055f1b0f7cb9ba66d0729705
brettb <brettbedarf.dev@gmail.com>
2021-03-17 22:11:50 -0600

    Add bootstrap and counter button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,7 @@ export class AppComponent {
   title = 'angular-essentials-udemy';
   rootName = 'BrettRoot';
   rootItems = ['Apples', 'Bananas', 'Cherries'];
+  number = 0;
 
   onNameChanged(newName: string) {
     this.rootName = newName;
@@ -17,5 +18,8 @@ export class AppComponent {
   onCartChanged(cartItem: string) {
     this.rootItems.push(cartItem);
     console.log(this.rootItems);
+  }
+  onIncreaseClick() {
+    this.number++;
   }
 }
